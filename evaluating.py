de7e0832bb3271edeedb6e15e44f01cf8e43ec06
Chayanika Basak <cb250802@gmail.com>
2022-07-10 13:53:29 +0530

    Models created, accuracy calculated

--- evaluating.py
+++ evaluating.py
@@ -1,15 +1,20 @@
 from sklearn.metrics import confusion_matrix, classification_report
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.metrics import accuracy_score
 
-def model_evaluate(model,X_test,y_test):
+plt.style.use('dark_background')
+
+def model_evaluate(model,X_test,y_test,font):
     
     # Predict values for Test dataset
     y_pred = model.predict(X_test) #Xtest is not used in model training
-
+    acc = accuracy_score(y_test, y_pred)*100
     # Print the evaluation metrics for the dataset.
-    print(classification_report(y_test, y_pred))
+    cr = classification_report(y_test, y_pred)
+    print(cr)
     
     # Compute and plot the Confusion matrix
     cf_matrix = confusion_matrix(y_test, y_pred)
@@ -27,3 +32,28 @@ def model_evaluate(model,X_test,y_test):
     plt.xlabel("Predicted values", fontdict = {'size':14}, labelpad = 10)
     plt.ylabel("Actual values"   , fontdict = {'size':14}, labelpad = 10)
     plt.title ("Confusion Matrix", fontdict = {'size':18}, pad = 20)
+    plt.show()
+    
+    l = cr.split('\n')
+    lines=[]
+    for i in range(len(l)):
+        if(len(l[i])!=0):
+            lines.append(l[i])
+    for i in range(len(lines)):
+        lines[i] = lines[i].strip()
+    ws=[]
+    for line in lines:
+        ws.append(line.split())
+    df_f1 = pd.DataFrame({'f1 score': pd.Series([ws[1][3],ws[2][3]])})
+    # df_f1['class']=df_f1['class'].astype('float')
+    df_f1['f1 score']=df_f1['f1 score'].astype('float')
+    
+    plt.figure(figsize=(10,5))
+    plt.title('f1_score',fontdict=font,pad=40)
+    plt.xticks(fontsize=16)
+    plt.yticks(fontsize=16)
+    sns.heatmap(df_f1,annot=True,annot_kws={'fontdict':font},robust=True)
+    plt.show()
+    
+    return acc
+
